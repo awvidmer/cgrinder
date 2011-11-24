763048d0b980ebf67430c5abab9c72b84a338260
Tony Vidmer <awvidmer@gmail.com>
2011-11-23 19:09:02 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cgrinder::Application.config.secret_token = '3c4d709adbfcdeddd2cd6f174aac2287c3b515335883b03805c4e84aef51d572f97e5036afd62c6415d3ee1a2311ec46ae6648f056ff994a789201eea0916894'
